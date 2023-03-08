239ced716126977a490a522ad5c95d0dcdd2a8eb
nyiriistvan <92971467+nyiriistvan@users.noreply.github.com>
2023-03-08 22:13:33 +0100

    Tanulók lekérése befejezve

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getStudents() {
+    let endpoint = 'students'
+    let url = environment.api + endpoint;
+    return this.http.get<any>(url);
+  }
+
+  getScores() {
+    let endpoint = 'scores'
+    let url = environment.api + endpoint;
+    return this.http.get<any>(url);
+  }
 }
--- src/app/tanulok/tanulok.component.ts
+++ src/app/tanulok/tanulok.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../shared/api.service';
 @Component({
   selector: 'app-tanulok',
   templateUrl: './tanulok.component.html',
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TanulokComponent implements OnInit {
 
-  constructor() { }
+  students:any = [];
+  scores:any = [];
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+    this.getStudents();
+  }
+
+  getStudents() {
+
+    this.api.getStudents().subscribe({
+      next: (students) => {
+        console.log(students)
+        this.students = students;
+      },
+      error: (err) => {
+        console.log('Hiba! A REST API elérése sikertelen!')
+        console.log(err)
+      }
+    });
+  }
+
+  getScores() {
+
+    this.api.getStudents().subscribe({
+      next: (scores) => {
+        console.log(scores)
+        this.students = scores;
+      },
+      error: (err) => {
+        console.log('Hiba! A REST API elérése sikertelen!')
+        console.log(err)
+      }
+    });
   }
 
 }
